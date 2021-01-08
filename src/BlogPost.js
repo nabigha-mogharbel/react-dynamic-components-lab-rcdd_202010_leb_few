91a48e512a75c819ea40a7ae334d13cf4ac92f33
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-08 23:52:04 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-// are we missing an import?
-import Comment from "./Comment"
+import Comment from "./Comment.js"
 
 export default class BlogPost extends Component {
   render() {
